d9af99271d4ce1d22d9f1e3262f684dd77f0fb21
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-19 14:08:12 +0200

    Set up message_channel.js to send a message to the server-side MessageChannel

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -1,18 +1,33 @@
-import consumer from "channels/consumer"
+import consumer from 'channels/consumer';
 
-const messageChannel = consumer.subscriptions.create("MessageChannel", {
+const messageChannel = consumer.subscriptions.create('MessageChannel', {
   connected() {
     // Called when the subscription is ready for use on the server
-    console.log('messageChannel.connected()')
+    console.log('messageChannel.connected()');
   },
 
   disconnected() {
     // Called when the subscription has been terminated by the server
-    console.log('messageChannel.disconnected()')
+    console.log('messageChannel.disconnected()');
   },
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-    console.log('messageChannel.received(data)')
+    console.log('messageChannel.received(data)');
+  },
+});
+
+document.addEventListener('turbo:load', () => {
+  let form = document.querySelector('#message-form');
+  if (form) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      let messageInput = document.querySelector('#message-input').value;
+      if (messageInput == '') return;
+      const message = {
+        body: messageInput,
+      };
+      messageChannel.send({ message: message });
+    });
   }
 });
