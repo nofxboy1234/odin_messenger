0c2d7823e3fdb010dad9f70432ed669266c13fbd
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-19 14:43:37 +0200

    Update debug prints

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -3,17 +3,17 @@ import consumer from 'channels/consumer';
 const messageChannel = consumer.subscriptions.create('MessageChannel', {
   connected() {
     // Called when the subscription is ready for use on the server
-    console.log('messageChannel.connected()');
+    console.log('**** messageChannel.connected() ****');
   },
 
   disconnected() {
     // Called when the subscription has been terminated by the server
-    console.log('messageChannel.disconnected()');
+    console.log('**** messageChannel.disconnected() ****');
   },
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-    console.log('messageChannel.received(data)');
+    console.log('**** messageChannel.received(data) ****');
     console.log(data);
 
     const messageDisplay = document.querySelector('#message-display');
